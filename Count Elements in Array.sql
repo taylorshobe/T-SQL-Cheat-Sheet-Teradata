554726cc0e4a3e6ee87816741e8d78f2ad859337
taylorshobe <44760142+taylorshobe@users.noreply.github.com>
2023-02-08 21:04:03 -0600

    Add files via upload

---
+++ Count Elements in Array.sql
@@ -0,0 +1,21 @@
+CREATE FUNCTION CountArrayElements (@Array VARCHAR(8000))
+RETURNS INTEGER
+BEGIN
+   DECLARE @Count INTEGER
+   SET @Count = 0
+   DECLARE @Element VARCHAR(8000)
+
+   WHILE CHARINDEX(',', @Array) > 0
+   BEGIN
+      SET @Element = SUBSTRING(@Array, 1, CHARINDEX(',', @Array) - 1)
+      SET @Array = SUBSTRING(@Array, CHARINDEX(',', @Array) + 1, LEN(@Array))
+      SET @Count = @Count + 1
+   END
+
+   SET @Count = @Count + 1
+
+   RETURN @Count
+END
+
+SELECT dbo.CountArrayElements('element1,element2,element3')
+
