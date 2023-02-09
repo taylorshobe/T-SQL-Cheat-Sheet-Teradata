c500a8fa84df0e4c9d658c3e900b44d8bd5bf901
taylorshobe <44760142+taylorshobe@users.noreply.github.com>
2023-02-08 21:27:19 -0600

    Add files via upload

---
+++ Count Elements in Array.sql
@@ -0,0 +1,43 @@
+
+Teradata
+---------------------------------------------------
+CREATE FUNCTION CountArrayElements (@Array VARCHAR(8000))
+RETURNS INTEGER
+BEGIN
+   DECLARE @Count INTEGER
+   SET @Count = 0
+   DECLARE @Element VARCHAR(8000)
+
+   WHILE CHARINDEX(',', @Array) > 0
+   BEGIN
+      SET @Element = SUBSTRING(@Array, 1, CHARINDEX(',', @Array) - 1)
+      SET @Array = SUBSTRING(@Array, CHARINDEX(',', @Array) + 1, LEN(@Array))
+      SET @Count = @Count + 1
+   END
+
+   SET @Count = @Count + 1
+
+   RETURN @Count
+END
+
+SELECT dbo.CountArrayElements('element1,element2,element3')
+
+SELECT dbo.CountArrayElements(column_name)
+FROM database.table
+
+
+--------------------------------------------
+HiveQL
+
+SELECT SIZE(SPLIT(column_name, ','))
+FROM table
+
+
+-- With NULLS
+SELECT SIZE(
+	CASE WHEN column_name IS NULL 
+	THEN array() 
+	ELSE SPLIT(column_name, ',') 
+	END
+	)
+FROM table
